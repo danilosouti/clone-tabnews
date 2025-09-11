f9a047c5c69b896c79e709c00420545cb8a61fa6
Danilo Souza <ddanilo.alex@gmail.com>
2025-09-11 17:16:58 +0000

    add component: vercel `Analytics` using `pages` directory

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import { Analytics } from '@vercel/analytics/next'
+
 function Home(){
-    return <h1>Oi! Cadê o Nikinho? Vem cá me dar um beijinho.</h1>
+    
+    return (
+    <>
+        <h1>Oi! Cadê o Nikinho? Vem cá me dar um beijinho.</h1>
+        <Analytics />
+    </>
+    );
+    
 }
 
 export default  Home
