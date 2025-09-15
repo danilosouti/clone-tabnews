afca3f0158b0965dcb9f52732b01bed02471055c
Danilo Souza <ddanilo.alex@gmail.com>
2025-09-15 20:35:19 +0000

    Add prettier script `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
-import { Analytics } from '@vercel/analytics/next'
+import { Analytics } from "@vercel/analytics/next";
 
-function Home(){
-    
-    return (
+function Home() {
+  return (
     <>
-        <h1>Olá! Agora com nova frase.</h1>
-        <Analytics />
+      <h1>Olá! Agora com nova frase.</h1>
+      <Analytics />
     </>
-    );
-    
+  );
 }
 
-export default  Home
+export default Home;
