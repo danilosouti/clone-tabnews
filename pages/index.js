5d53bad8b516f89a2f913bc9f787fb761998e29f
Danilo Souza <ddanilo.alex@gmail.com>
2025-09-24 21:25:22 +0000

    Add Vercel Speed Insights

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function Home() {
   return (
     <>
       <h1>Olá! Agora com nova frase.</h1>
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
